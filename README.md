1abf36e6e15a7080d571670459b65d37d5ddcd36
David Brown <github@scri.pt>
2013-06-16 16:04:14 -0700

    Initial check-in.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 kujua-transport
 ===============
 
-A shared SMS transport layer, for use with Medic Mobile's Kujua Lite and Kujua v1 software.
+Shared SMS transport layer, for use with Medic Mobile's Kujua Lite and Kujua v1
---
+++ node_modules/kujua-transport/lib/driver/abstract.js
@@ -0,0 +1,115 @@
+
+/**
+ * @namespace abstract:
+ */
+exports.prototype = {
+
+  /**
+   * @name initialize:
+   *   Perform device-specific or API-specific initialization.
+   */
+  initialize: function (_options) {
+
+    this._options = (_options || {});
+    return this;
+  },
+
+  /**
+   * @name send:
+   *   Send a new message. The `_message` argument must be an object,
+   *   containing at least a `to` property (set to the phone number or
+   *   MSISDN of the intended recipient), and a `content` property
+   *   (containing the message body, encoded as utf-8 text).
+   *
+   *   After the message has been successfully transmitted, the
+   *   `_callback(_err, _result)` function will be invoked with two
+   *   arguments -- `_err` will be a node.js-style error object (or
+   *   false-like if no error occurred). THe `_result` argument will be
+   *   an object containing, at a minimum, a single `result` property
+   *   set to either `success`, `partial`, or `failure`. A result of
+   *   `partial` indicates that the message was too large for a single
+   *   message, had to be fragmented, and one or more of the fragments
+   *   failed to send properly.
+   *
+   *   This function should return the driver instance in `this`.
+   */
+  send: function (_message, _callback) {
+
+    if (this._options.debug) {
+      process.stderr.write(
+        'abstract driver: sent ' + JSON.stringify(_message) + '\n'
+      );
+    }
+
+    _callback.call(
+      this, null, { status: 'success', abstract: true }
+    );
+
+    return this;
+  },
+
+  /**
+   * @name register_receive_handler:
+   *   Ask the driver to invoke `_callback` whenever a message arrives.
+   *   The `_handler(_message, _callback)` function will be invoked
+   *   for each message received: the `_err` argument is a node.js-style
+   *   error object (or false-like if the operation was successful); the
+   *   `_message` argument is an object containing at least the `from`,
+   *   `timestamp`, and `content` properties; the `_callback(_err)` argument
+   *   is a function that must be called by our instansiator once the
+   *   message has been safely written to persistent storage. If for some
+   *   reason our instansiator cannot accept the message, the function
+   *   should still be called, but the `_err` parameter set to a non-null
+   *   error object.
+   */
+  register_receive_handler: function (_handler) {
+
+    return this;
+  },
+
+  /**
+   * @name register_error_handler:
+   *   Ask the driver to invoke `_handler(_err)` whenever an error occurs
+   *   that cannot be attributed to a requested `send` operation. The
+   *   `_err` argument will contain a node.js-style error object, and
+   *   should never be null.
+   */
+  register_error_handler: function (_handler) {
+
+    return this;
+  },
+
+  /**
+   * @name start:
+   *   Start any polling and/or watch operations that are required
+   *   for this driver instance to function properly. To avoid data
+   *   loss, this function *must* be called after you've registered
+   *   callback functions via the `register_receive_handler` and
+   *   `register_error_handler` methods.
+   */
+  start: function () {
+
+    return this;
+  },
+
+  /**
+   * @name stop:
+   *   Stop any polling and/or watch operations that are currently
+   *   running on behalf of this driver instance.
+   */
+  stop: function () {
+
+    return this;
+  },
+
+  /**
+   * @name destroy:
+   *   Release any and all resources held by this driver.
+   */
+  destroy: function () {
+
+    return this.stop();
+  }
+
+};
+
