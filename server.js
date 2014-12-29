4fbb12d0c7411298230cd318d2bf5bb4f94fa328
Lucas Manfield <lucas@medicmobile.org>
2014-12-29 11:55:15 -0800

    potential fix to 559, added support for multiple adapters/drivers in transport
    you can rip out the M:M in transport if its unnecessary, but it seems
    reasonable to me

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var factory = require('./lib/factory');
-var transport = factory.create({
-    name: 'smssync',
-    debug: true,
-    secret: 'secret'
-  },
-  {
-    name: 'medic-mobile',
-    debug: true,
-    user: 'admin',
-    password: 'password',
-    url: 'http://localhost:5984/medic/_design/medic/_rewrite'
-  }
+var transport = factory.create([
+    {
+      name: 'smssync',
+      debug: true,
+      secret: 'secret'
+    }
+  ],
+  [
+    {
+      name: 'medic-mobile',
+      debug: true,
+      user: 'admin',
+      password: 'password',
+      url: 'http://localhost:5984/medic/_design/medic/_rewrite'
+    }
+  ]
 );
 transport.start();
