1815d31ede29326322305f9a8ac71258a80ddb12
Lucas Manfield <lucas@medicmobile.org>
2015-01-14 15:56:27 -0800

    twilio driver + status phone number fixes

---
+++ examples/twilio.js
@@ -0,0 +1,26 @@
+var factory = require('../lib/factory');
+var transport = factory.create([{
+  name: 'twilio',
+  sid: 'ACc27ce8143c2f05d804a4828fc0f0f546',
+  token: '986200d750694da9647988be4372bb51',
+  phone: '+1 650-276-3342',
+  log_level: 'debug'
+}],
+{
+  name: 'medic-mobile',
+  user: 'admin',
+  password: 'password',
+  url: 'http://localhost:5984/medic/_design/medic/_rewrite',
+  log_level: 'debug'
+},
+{
+  name: 'simple',
+  log_level: 'debug'
+},
+{
+  default_country_code: '1',
+  status_check_interval: 1, //default to every minute for testing
+  log_level: 'debug'
+}
+);
+transport.start();
