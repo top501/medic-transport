bd442bca238329a60997aea491e6189659e7c23e
Lucas Manfield <lucas@medicmobile.org>
2015-02-02 15:14:50 -0800

    lets try this again

--- examples/medic-os.js
+++ examples/medic-os.js
@@ -5,5 +5,8 @@ if (process.argv.length < 3) {
 } else {
   file_name = process.argv[2];
 }
-var transport = factory.create_from_config_file(file_name);
+var config = factory.create_config_from_file(file_name)
+config.adaptor.user = 'admin';
+config.adaptor.password = 'pass';
+var transport = factory.create_from_config(config);
 transport.start();
